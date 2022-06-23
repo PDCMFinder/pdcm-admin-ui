277c389396d604b1959d6c4fa296b81321b604df
Mauricio Martinez <mauroz77@gmail.com>
2022-06-23 15:42:45 +0100

    Create OntologyLoader component to provide an independent button to reload the ontologies and inform about the result

--- src/apis/Ontologies.api.js
+++ src/apis/Ontologies.api.js
@@ -1,7 +1,9 @@
-const API_URL = process.env.REACT_APP_API_URL;
 
-console.log('API_URL', API_URL);
-
+/**
+ * Get a summary of the loaded ontologies
+ * @returns Json with counts by type and total records, as well as information about the loading
+ * process.
+ */
 export async function getOntologySummary() {
     let response = await fetch(
         `${process.env.REACT_APP_API_URL}/api/ontology/getSummary`
@@ -11,3 +13,18 @@ export async function getOntologySummary() {
     }
     return response.json()
 }
+
+/**
+ * Loads the ontologies from OLS
+ * @returns Information about the loading process.
+ */
+export async function loadOntologies() {
+    let response = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/ontology/loadOntologies`
+    );
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+    return response.json()
+}
+
